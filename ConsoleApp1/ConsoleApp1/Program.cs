33bab24c3fefa1244a988e8cd73eb0c959f0f246
lubimov.v <lubimov.work@gmail.com>
2021-04-10 12:28:11 +0300

    Add colors

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,15 +6,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Gray;
             int action;
             while(true)
             {
                 Console.Clear();
 
-                Console.WriteLine("[1] Логин");
-                Console.WriteLine("[2] Регистрация");
-                Console.WriteLine("[3] Выход");
-
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\t[1] Логин");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\t[2] Регистрация");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("\t[3] Выход");
+                Console.ForegroundColor = ConsoleColor.Gray;
                 int.TryParse(Console.ReadLine(), out action);
             }
         }
