56cac14029986c21a876e9fc31ad864643bf9b9a
lubimov.v <lubimov.work@gmail.com>
2021-04-10 12:20:31 +0300

    Add project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int action;
+            while(true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("[1] Логин");
+                Console.WriteLine("[2] Регистрация");
+                Console.WriteLine("[3] Выход");
+
+                int.TryParse(Console.ReadLine(), out action);
+            }
+        }
+    }
+}
