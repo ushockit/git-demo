58fa09becf6893e778a0b9d1e8ef74e59428985f
lubimov.v <lubimov.work@gmail.com>
2021-04-10 12:55:40 +0300

    Remove colors

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,37 +6,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Gray;
             int action;
             while(true)
             {
                 Console.Clear();
 
-                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("\t[1] Логин");
-                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("\t[2] Регистрация");
-                Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("\t[3] Выход");
-                Console.ForegroundColor = ConsoleColor.Gray;
                 int.TryParse(Console.ReadLine(), out action);
 
 
                 switch (action)
                 {
                     case 1:
-                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Авторизация в системе");
                         break;
                     case 2:
-                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Регистрация в системе");
                         break;
                     case 3:
                         Environment.Exit(0);
                         break;
                 }
-                Console.ForegroundColor = ConsoleColor.Gray;
             }
         }
     }
